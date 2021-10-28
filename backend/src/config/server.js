fb9fe62e9cc547f06ce327e7008788f6ed396386
Matheus Rodrigues <matheusrf96@gmail.com>
2021-10-28 10:48:57 -0300

    Implement cors

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,8 +4,11 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 
+const allowCors = require('./cors')
+
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, () => {
     console.log(`backend is listening on port ${ port }`)
