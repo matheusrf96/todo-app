ed1110330db609cfb35d5e775091dfcd14c45e18
Matheus Rodrigues <matheusrf96@gmail.com>
2021-10-28 16:16:31 -0300

    Create app and index

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,11 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+const App = props => (
+    <div className="container">
+        <h1>Teste</h1>
+    </div>
+)
+export default App
