7a2f41947875eec269fe1944f42e7ec810ea2b53
Matheus Rodrigues <matheusrf96@gmail.com>
2021-11-01 16:34:10 -0300

    Fix dependencies problems

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,9 +1,10 @@
-import 'modules/bootstrap/dist/css/bootstrap.min.css'
-import 'modules/font-awesome/css/font-awesome.min.css'
-
 import React from 'react'
 
-const App = props => (
+require('bootstrap/dist/css/bootstrap.min.css')
+require('font-awesome/css/font-awesome.min.css')
+
+
+const App = (props) => (
     <div className="container">
         <h1>Teste</h1>
     </div>
