3981257332f06271879fc395ce54fbfb33fb2006
Matheus Rodrigues <matheusrf96@gmail.com>
2021-11-01 17:33:14 -0300

    Create PageHeader component

--- frontend/src/about/About.jsx
+++ frontend/src/about/About.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React from "react"
+
+import PageHeader from "../template/PageHeader"
 
 const About = (props) => {
     return (
         <div>
-            <h1>Sobre</h1>
+            <PageHeader name="Sobre" small="Nós" />
+
+            <h2>Nossa História</h2>
+            <p>Lorem ipsum dolor sit amet...</p>
         </div>
     )
 }
--- frontend/src/todo/Todo.jsx
+++ frontend/src/todo/Todo.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
+import PageHeader from "../template/PageHeader";
+
 class Todo extends Component {
     render() {
         return (
             <div>
-                <h1>Todo</h1>
+                <PageHeader name="Tarefas" small="Cadastro" />
             </div>
         )
     }
