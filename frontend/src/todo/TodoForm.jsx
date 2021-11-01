ce8747bf8aaa408c7a2fcb08ad5812d96002f268
Matheus Rodrigues <matheusrf96@gmail.com>
2021-11-01 18:00:42 -0300

    Implement TodoForm and TodoList components

---
+++ frontend/src/todo/TodoForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoForm = (props) => {
+    return (
+        <div>
+            <h1>Form</h1>
+        </div>
+    )
+}
+
+export default TodoForm
---
+++ frontend/src/todo/TodoList.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const TodoList = (props) => {
+    return (
+        <h1>List</h1>
+    )
+}
+
+export default TodoList
