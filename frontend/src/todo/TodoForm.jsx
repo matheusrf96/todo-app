b960b24f99f9a4d98798e5492cdc1914f63ee423
Matheus Rodrigues <matheusrf96@gmail.com>
2021-11-02 17:51:43 -0300

    Implement IconButton and Grid components

--- frontend/src/todo/TodoForm.jsx
+++ frontend/src/todo/TodoForm.jsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React from "react"
+
+import Grid from "../template/Grid"
+import IconButton from "../template/IconButton"
 
 const TodoForm = (props) => {
     return (
         <div role="form" className="todo-form">
-            <div className="col-xs-12 col-sm-9 col-md-10">
+            <Grid cols='12 9 10'>
                 <input type="text" id="description" className="form-control"
                     placeholder="Adicione uma tarefa" />
-            </div>
+            </Grid>
 
-            <div className="col-xs-12 col-sm-3 col-md-2">
-                <button className="btn btn-primary">
-                    <i className="fa fa-plus"></i>
-                </button>
-            </div>
+            <Grid cols='12 3 2'>
+                <IconButton style="primary" icon="plus" />
+            </Grid>
         </div>
     )
 }
