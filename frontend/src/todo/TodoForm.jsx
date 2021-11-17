c0dc138a91f5939c0012b4b69922ae915ede4417
Matheus Rodrigues <matheusrf96@gmail.com>
2021-11-17 16:25:40 -0300

    Apply redux promise middleware

--- frontend/src/todo/TodoForm.jsx
+++ frontend/src/todo/TodoForm.jsx
@@ -1,45 +1,58 @@
-import React from "react"
+import React, { Component } from "react"
 import { connect } from "react-redux"
 import { bindActionCreators } from "redux"
 
 import Grid from "../template/Grid"
 import IconButton from "../template/IconButton"
-import { changeDescription } from "./todoActions"
+import { changeDescription, search } from "./todoActions"
 
-const TodoForm = (props) => {
-    const keyHandler = (e) => {
+
+class TodoForm extends Component {
+    constructor(props) {
+        super(props)
+        this.keyHandler = this.keyHandler.bind(this)
+    }
+
+    componentWillMount() {
+        this.props.search()
+    }
+
+    keyHandler(e) {
         if (e.key === 'Enter') {
-            e.shiftKey ? props.handleSearch() : props.handleAdd()
+            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
         }
         else if (e.key === 'Escape') {
-            props.handleClear()
+            this.props.handleClear()
         }
     }
 
-    return (
-        <div role="form" className="todo-form">
-            <Grid cols='12 9 10'>
-                <input
-                    type="text"
-                    id="description"
-                    className="form-control"
-                    placeholder="Adicione uma tarefa"
-                    onChange={ props.changeDescription }
-                    onKeyUp={ keyHandler }
-                    value={ props.description }
-                />
-            </Grid>
+    render() {
+        return (
+            <div role="form" className="todo-form">
+                <Grid cols='12 9 10'>
+                    <input
+                        type="text"
+                        id="description"
+                        className="form-control"
+                        placeholder="Adicione uma tarefa"
+                        onChange={ this.props.changeDescription }
+                        onKeyUp={ this. keyHandler }
+                        value={ this.props.description }
+                    />
+                </Grid>
 
-            <Grid cols='12 3 2'>
-                <IconButton onClick={ props.handleAdd } style="primary" icon="plus" />
-                <IconButton onClick={ props.handleSearch } style="info" icon="search" />
-                <IconButton onClick={ props.handleClear } style="default" icon="close" />
-            </Grid>
-        </div>
-    )
+                <Grid cols='12 3 2'>
+                    <IconButton onClick={ this.props.handleAdd } style="primary" icon="plus" />
+                    <IconButton onClick={ this.props.handleSearch } style="info" icon="search" />
+                    <IconButton onClick={ this.props.handleClear } style="default" icon="close" />
+                </Grid>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state) => ({ description: state.todo.description })
-const mapDispatchToProps = (dispatch) => bindActionCreators({ changeDescription }, dispatch)
+const mapDispatchToProps = (dispatch) =>
+    bindActionCreators({ changeDescription, search }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,26 +1,11 @@
-const INITIAL_STATE = {
-    description: 'Ler livro',
-    list: [{
-        _id: 1,
-        description: 'Pagar fatura do cartão',
-        done: true,
-    },
-    {
-        _id: 2,
-        description: 'Reunião',
-        done: false,
-    },
-    {
-        _id: 3,
-        description: 'Consulta',
-        done: false,
-    }],
-}
+const INITIAL_STATE = { description: '', list: [], }
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case 'DESCRIPTION_CHANGED':
             return { ...state, description: action.payload }
+        case 'TODO_SEARCHED':
+            return { ...state, list: action.payload.data }
         default:
             return state
     }
